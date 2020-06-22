b8111b58a0c06fd64423c3d41a25e730da71d73b
Shoaib-78637 <syedshoaibabbas786@gmail.com>
2020-06-22 10:10:06 +0500

    Remove UseCallBack

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext,useCallback} from 'react'
+import React, {useState, useContext} from 'react'
 import {GlobalContext} from './GlobalState'
 
  function AddTransaction() {
