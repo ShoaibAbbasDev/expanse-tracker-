02cdb329ee41ec3d367e626765df24ba2848e5e6
Dawoodjaveed <54910044+Dawoodjaveed@users.noreply.github.com>
2020-10-29 20:20:53 +0500

    Update Header.js
    header name edit

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 function Header  () {
-    return (
+    return ( 
         <h2>
-            Expense Tracker
+            Expense Tracker App  //adding commemt expanse tracker app
         </h2>
     );
 }
