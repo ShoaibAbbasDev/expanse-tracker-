5a688363443f4ae1174aecb8776705f62bd8a277
M. Wajahat Shahid <71876842+dr-wajahat@users.noreply.github.com>
2021-10-23 02:51:23 +0500

    UI improved and fix the spelling mistake

--- src/AddTransaction.js
+++ src/AddTransaction.js
@@ -1,42 +1,49 @@
-import React, {useState, useContext} from 'react'
-import {GlobalContext} from './GlobalState'
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "./GlobalState";
 
- function AddTransaction() {
+function AddTransaction() {
+	const [text, setText] = useState("");
+	const [amount, setAmount] = useState(0);
+	const { addTransaction } = useContext(GlobalContext);
+	const handleAddition = (event) => {
+		event.preventDefault();
 
-    const [text,setText]=useState('');
-    const [amount,setAmount]=useState(0);
-    const {addTransaction}=useContext(GlobalContext);
-    const handleAddition = (event) =>{
-        event.preventDefault();
+		const newTransaction = {
+			id: Math.floor(Math.random() * 10000000),
+			text,
+			amount: +amount,
+		};
+		addTransaction(newTransaction);
+	};
 
-        const newTransaction={
-            id:  Math.floor(Math.random() * 10000000),
-            text,
-            amount: + amount
-        }
-        addTransaction(newTransaction);
+	return (
+		<div>
+			<h3>Add New Transaction</h3>
+			<form onSubmit={handleAddition}>
+				<label htmlFor="text">
+					<span class="red">*</span>Description
+				</label>
+				<input
+					type="text"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+					placeholder="Description "
+				/>
 
-      }
-   
+				<label className="div-amount" htmlFor="amount">
+					<span class="red">*</span>Amount (negative - expanse ,
+					positive - income)
+				</label>
+				<input
+					type="number"
+					value={amount}
+					onChange={(e) => setAmount(e.target.value)}
+					placeholder="*Amount"
+				/>
 
-    return (
-        <div>
-            <h3>Add New Transaction</h3>
-            <form onSubmit={handleAddition}>
-                
-                    <label htmlFor="text">*Description</label>
-                    <input type="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="*Description "/>
-
-                
-                    <label className="div-amount" htmlFor="amount">*Aomount<br></br>(negative  - expanse , positive - income)</label>
-                    <input type="number" value={amount} onChange={(e)=>setAmount(e.target.value)} placeholder="*Amount" />
-               
-                <button className="btn"  >Add Transaction</button>
-              
-            </form>
-
-            </div>
-        
-    )
+				<button className="btn">Add Transaction</button>
+			</form>
+		</div>
+	);
 }
 export default AddTransaction;
